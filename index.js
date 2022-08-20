e0cb0eab8292181419df453e0f4b6a92bf7bd425
VTRyo <vtryo.work@gmail.com>
2022-08-20 23:02:51 +0900

    add schema. select id

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 const { ApolloServer, gql } = require('apollo-server');
 
+// String! means that the field is non-nullable
 const typeDefs = gql`
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+  }
+
   type Query{
     hello(name: String!): String
+    users: [User]
+    user(id: ID!): User
   }
 `;
+
+const users = [
+  { id: '1', name: 'VTRyo', email: 'VTRyo@example.com'},
+  { id: '2', name: 'Kushima', email: 'Kushima@example.com'},
+];
 
 const resolvers = {
   Query: {
     hello: (parent, args) => `Hello ${args.name}`,
+    users: ()  => users,
+    user: (parent, args) => {
+      const user = users.find((user) => user.id === args.id);
+      return user;
+    },
   },
 };
 
