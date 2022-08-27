d0dbe6e10762a809594aeb71ffc093806e514502
VTRyo <vtryo.work@gmail.com>
2022-08-27 23:46:20 +0900

    プロジェクト中のアイテムを取ってくる

--- github.sh
+++ github.sh
@@ -1,13 +1,75 @@
 #!/bin/bash
 
 # exec local machine
-gh api graphql -f query=\
-'query {
-   user(login: "VTRyo") {
-    contributionsCollection {
-      contributionCalendar {
-        totalContributions
+gh api graphql -f query='
+query{
+    node(id: "") {
+        ... on ProjectV2 {
+          items(first: 20) {
+            nodes{
+              id
+              fieldValues(first: 8) {
+                nodes{
+                  ... on ProjectV2ItemFieldTextValue {
+                    text
+                    field {
+                      ... on ProjectV2FieldCommon {
+                        name
+                      }
+                    }
+                  }
+                  ... on ProjectV2ItemFieldDateValue {
+                    date
+                    field {
+                      ... on ProjectV2FieldCommon {
+                        name
+                      }
+                    }
+                  }
+                  ... on ProjectV2ItemFieldSingleSelectValue {
+                    name
+                    field {
+                      ... on ProjectV2FieldCommon {
+                        name
+                      }
+                    }
+                  }
+                }
+              }
+              content{
+                ... on DraftIssue {
+                  title
+                  body
+                }
+                ...on Issue {
+                  title
+                  assignees(first: 10) {
+                    nodes{
+                      login
+                    }
+                  }
+                }
+                ...on PullRequest {
+                  title
+                  assignees(first: 10) {
+                    nodes{
+                      login
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
       }
-    }
-  }
-}'
+    }'
+
+# 'query {
+#    user(login: "VTRyo") {
+#     contributionsCollection {
+#       contributionCalendar {
+#         totalContributions
+#       }
+#     }
+#   }
+# }'
