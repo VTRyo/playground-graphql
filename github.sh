bc838651ea47e52a7413725baf7efc31040b6163
VTRyo <vtryo.work@gmail.com>
2022-08-28 12:53:24 +0900

    get issue from label name

--- github.sh
+++ github.sh
@@ -3,57 +3,19 @@
 # exec local machine
 gh api graphql -f query='
 query{
-    node(id: "") {
-        ... on ProjectV2 {
-          items(first: 20) {
-            nodes{
-              id
-              fieldValues(first: 8) {
-                nodes{
-                  ... on ProjectV2ItemFieldTextValue {
-                    text
-                    field {
-                      ... on ProjectV2FieldCommon {
-                        name
+  node(id: "") {
+    ... on ProjectV2 {
+      items(first: 20) {
+        nodes{
+          fieldValues(first: 10) {
+            nodes {
+              ... on ProjectV2ItemFieldLabelValue {
+                labels(first: 10) {
+                  nodes {
+                    issues(last: 10,labels: "onboard") {
+                      nodes {
+                        title
                       }
-                    }
-                  }
-                  ... on ProjectV2ItemFieldDateValue {
-                    date
-                    field {
-                      ... on ProjectV2FieldCommon {
-                        name
-                      }
-                    }
-                  }
-                  ... on ProjectV2ItemFieldSingleSelectValue {
-                    name
-                    field {
-                      ... on ProjectV2FieldCommon {
-                        name
-                      }
-                    }
-                  }
-                }
-              }
-              content{
-                ... on DraftIssue {
-                  title
-                  body
-                }
-                ...on Issue {
-                  title
-                  assignees(first: 10) {
-                    nodes{
-                      login
-                    }
-                  }
-                }
-                ...on PullRequest {
-                  title
-                  assignees(first: 10) {
-                    nodes{
-                      login
                     }
                   }
                 }
@@ -62,14 +24,7 @@ query{
           }
         }
       }
-    }'
-
-# 'query {
-#    user(login: "VTRyo") {
-#     contributionsCollection {
-#       contributionCalendar {
-#         totalContributions
-#       }
-#     }
-#   }
-# }'
+    }
+  }
+}
+'
