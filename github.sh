40c4997befdf51b55fa7f29f3da114dab8027483
VTRyo <vtryo.work@gmail.com>
2022-08-28 14:05:05 +0900

    get issue from toil labels

--- github.sh
+++ github.sh
@@ -5,26 +5,27 @@ gh api graphql -f query='
 query{
   node(id: "") {
     ... on ProjectV2 {
-      items(first: 20) {
-        nodes{
-          fieldValues(first: 10) {
-            nodes {
-              ... on ProjectV2ItemFieldLabelValue {
-                labels(first: 10) {
-                  nodes {
-                    issues(last: 10,labels: "onboard") {
-                      nodes {
-                        title
-                      }
-                    }
-                  }
+      items(first: 50) {
+        nodes {
+          content {
+            ... on Issue {
+              title
+              labels(first: 10) {
+                nodes {
+                  name
                 }
               }
             }
           }
         }
+
       }
     }
   }
 }
-'
+' > res.txt
+
+echo "==Toil task=="
+cat res.txt | jq -r '.data.node.items.nodes[].content | select(.labels.nodes[]?.name == "toil") | [.title]'
+echo "==Toil Resolve task=="
+cat res.txt | jq -r '.data.node.items.nodes[].content | select(.labels.nodes[]?.name == "toil/resolve") | [.title]'
